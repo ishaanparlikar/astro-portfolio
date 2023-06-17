2a73f5d9e693b01815fb29503622f8a08267927a
ishaanparlikar <iparlika@emich.edu>
2023-06-17 17:48:39 +0530

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,8 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: ['class', '[data-theme="dark"]'],
-  theme: {
-    colors: {
-      primary: 'var(--primary)',
-      secondary: 'var(--secondary)',
-      accent: 'var(--accent)',
-      neutral: 'var(--neutral)'
-    },
-
-
-    extend: {
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [
+    require('@catppuccin/tailwindcss'),
+  ],
 }
