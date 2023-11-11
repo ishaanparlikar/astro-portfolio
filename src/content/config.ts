2cfcab2a78d51dd8c00af94b61d3be4926218ae2
Ishan Parlikar <iparlika@emich.edu>
2023-11-11 19:06:01 +0530

    config file update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,25 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const aboutCollection = defineCollection({
+  type: "content",
   schema: z.object({
-    title: z.string(),
+    name: z.string(),
     image: z.string().optional(),
+    description: z.string(),
+  }),
+});
+
+const workCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    companyName: z.string(),
+    position: z.string(),
+    from: z.string(),
+    to: z.string(),
   }),
 });
 
 export const collections = {
-  profile: aboutCollection,
+  about: aboutCollection,
+  work: workCollection,
 };
