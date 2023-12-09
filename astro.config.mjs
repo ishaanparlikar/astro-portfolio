48e67634d0f9d465f7941a9483e4034d86cbaf93
Ishan Parlikar <iparlika@emich.edu>
2023-12-09 16:57:53 +0530

    added astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import preact from "@astrojs/preact";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +8,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  })]
+  }), 
+    icon()
+  ]
 });
